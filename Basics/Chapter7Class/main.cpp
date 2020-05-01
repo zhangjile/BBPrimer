30c5a7acd2f5cbde8b18b5adb76f3d3e7f2db8bb
Alvin-oy <schwabiz@yahoo.com>
2020-05-01 14:04:29 +0800

    Bookstore project
    v1.0

--- Basics/Chapter7Class/main.cpp
+++ Basics/Chapter7Class/main.cpp
@@ -1,7 +1,12 @@
-//Absolutely no special characters in project names or worrkspace name
-#include <iostream>
+//cmake AddItems, 
+//AddItem <infile >outfile
+# include <iostream>
+# include "Sales_item.h"
+using std::cin; using std::cout; using std::endl;
 
 int main() {
-    std::cout << "be consistent" <<std::endl;
+    Sales_item item1, item2;
+    cin >> item1 >>item2;
+    cout << item1 + item2 <<endl;
     return 0;
 }
