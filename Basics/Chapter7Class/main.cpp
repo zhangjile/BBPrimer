448640e958844e99dfc32b211bf0557ed7db3431
Alvin-oy <schwabiz@yahoo.com>
2020-05-01 15:06:34 +0800

    ifstream v2.0
    read from a data file instead of keyboard

--- Basics/Chapter7Class/main.cpp
+++ Basics/Chapter7Class/main.cpp
@@ -1,12 +1,29 @@
-//cmake AddItems, 
-//AddItem <infile >outfile
-# include <iostream>
-# include "Sales_item.h"
-using std::cin; using std::cout; using std::endl;
+//fstream, v2.0
+#include <iostream>
+#include <fstream>
+#include "Sales_item.h"
+using std::cin; using std::cout; using std::endl; using std::ifstream; using std::cerr;
 
 int main() {
-    Sales_item item1, item2;
-    cin >> item1 >>item2;
-    cout << item1 + item2 <<endl;
-    return 0;
+    Sales_item item1;
+    ifstream Read ("book_sales");
+    if(Read >>item1){
+        Sales_item item2;
+        while(Read >> item2){
+            if(item1.isbn() == item2.isbn()){
+                item1 += item2;
+            } else{
+                cout << item1 <<endl;
+                item1 = item2;
+            }
+            
+        }
+        cout << item1 <<endl;
+        return 0;
+    } else{
+        cerr <<"Error" <<endl;
+        return -1;
+    }
+    Read.close();
+    
 }
