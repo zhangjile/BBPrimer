fb6f1b04d16cb14c0b67f3195498cdc306e345e8
Alvin-oy <zhangjile@gmail.com>
2020-04-30 22:45:13 -0400

    Basics chapter 7 class uniform

---
+++ Basics/Chapter7Class/main.cpp
@@ -0,0 +1,7 @@
+//Absolutely no special characters in project names or worrkspace name
+#include <iostream>
+
+int main() {
+    std::cout << "be consistent" <<std::endl;
+    return 0;
+}
--- Basics/Chapter_7/main.cpp
+++
@@ -1,8 +0,0 @@
-//NO special characters in workspace name!
-//Chapter 7 of Primer
-#include <iostream>
-
-int main() {
-    std::cout << "the bug fixed finally!" <<std::endl;
-    return 0;
-}
