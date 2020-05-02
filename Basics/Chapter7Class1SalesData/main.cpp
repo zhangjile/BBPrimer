a81b7209f3d3e2ac4c8aa416e054229eaf331875
Alvin-oy <schwabiz@yahoo.com>
2020-05-02 21:41:13 +0800

    fstream v2.0
    cout is an object of ostream

--- Basics/Chapter7Class1SalesData/main.cpp
+++ Basics/Chapter7Class1SalesData/main.cpp
@@ -1,6 +1,6 @@
-//section 7.1 defining abstract data types (Ex 7.3, p 260)
-//add Combine and isbn members to the SalesData class and revise transaction processing program
-// the 3rd item in book_sales file is taken as Revenue
+//section 7.1 defining abstract data types (Ex 7.7, p 262)
+//add Read function
+// the 3rd item in book_sales file is Price. Revenue is correctly calculated now
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -12,6 +12,7 @@ struct SalesData {
 	double Revenue = 0.0;
 	std::string isbn () const {return BookNO;}
 	SalesData& Combine (const SalesData &item);
+
 };
 
 SalesData& SalesData::Combine(const SalesData &item){
@@ -20,20 +21,51 @@ SalesData& SalesData::Combine(const SalesData &item){
 	return *this;
 }
 
+ifstream &Read (ifstream &is, SalesData &item);
+ostream &Write (ostream &os, const SalesData &item);
+ofstream &Save (ofstream &os, const SalesData &item);
+SalesData& Add (const SalesData item1, const SalesData item2);
+
+ifstream &Read (ifstream &is, SalesData &item){
+	double Price {0.0};
+	is >> item.BookNO >> item.UnitsSold >> Price;
+	item.Revenue = item.UnitsSold * Price;
+	return is;
+}
+
+ofstream &Save (ofstream &os, const SalesData &item){
+	os << item.BookNO <<"\t" <<item.UnitsSold <<"\t" <<item.Revenue;
+	return os;
+}
+
+ostream &Write (ostream &os, const SalesData &item){
+	os << item.BookNO <<"\t" <<item.UnitsSold <<"\t" <<item.Revenue;
+	return os;
+}
+
+SalesData& Add (const SalesData item1, const SalesData item2){
+	SalesData temp = item1;
+	temp.Combine(item2);
+	return temp;
+}
+
+
+
 int main(){
 	std::ifstream items {"book_sales"};
-	SalesData total,Single;
-	if(items>>total.BookNO >>total.UnitsSold >>total.Revenue){
-		while (items>>Single.BookNO >>Single.UnitsSold >>Single.Revenue){
-			if(total.BookNO == Single.BookNO){
-				total.Combine(Single);
+	ofstream yeah {"book_result"};
+	SalesData Total,Single;
+	if(Read(items,Total)){
+		while (Read(items,Single)){
+			if(Total.BookNO == Single.BookNO){
+				Total.Combine(Single);
 			}
 			else{
-				cout<< total.BookNO <<"\t"<<total.UnitsSold <<"\t"<<total.Revenue <<endl;
-				total = Single;
+				Write(cout,Total)<<endl; //wow, just wow for cout:)
+				Total = Single;
 			}
 		}
-		cout<< total.BookNO <<"\t"<<total.UnitsSold <<"\t"<<total.Revenue <<endl;
+		Save(yeah, Total);
 	}
 	else{
 		cerr <<"no data";
