107950524e2c8c519ed4d904b0dddc27ea43923a
Alvin-oy <schwabiz@yahoo.com>
2020-05-03 15:53:21 +0800

    access control and encapsulation
    .h

---
+++ Basics/Chapter7Class1SalesData/SalesData.h
@@ -0,0 +1,50 @@
+#ifndef SALESDATA_H
+#define SALESDATA_H
+#include <iostream>
+#include <fstream>
+#include <string>
+
+class SalesData {
+	private:
+	std::string BookNO;
+	unsigned UnitsSold = 0;
+	double Revenue = 0.0;
+	
+	public:
+	std::string isbn () const {return BookNO;}
+	SalesData& Combine (const SalesData &item);
+	
+	friend std::ifstream &Read (std::ifstream &is, SalesData &item);
+	friend std::ostream &Write (std::ostream &os, const SalesData &item);
+	friend std::ofstream &Save (std::ofstream &os, const SalesData &item);
+
+};
+
+SalesData& SalesData::Combine(const SalesData &item){
+	UnitsSold += item.UnitsSold;
+	Revenue += item.Revenue;
+	return *this;
+}
+
+std::ifstream &Read (std::ifstream &is, SalesData &item);
+std::ostream &Write (std::ostream &os, const SalesData &item);
+std::ofstream &Save (std::ofstream &os, const SalesData &item);
+
+std::ifstream &Read (std::ifstream &is, SalesData &item){
+	double Price {0.0};
+	is >> item.BookNO >> item.UnitsSold >> Price;
+	item.Revenue = item.UnitsSold * Price;
+	return is;
+}
+
+std::ofstream &Save (std::ofstream &os, const SalesData &item){
+	os << item.BookNO <<"\t" <<item.UnitsSold <<"\t" <<item.Revenue;
+	return os;
+}
+
+std::ostream &Write (std::ostream &os, const SalesData &item){
+	os << item.BookNO <<"\t" <<item.UnitsSold <<"\t" <<item.Revenue;
+	return os;
+}
+
+#endif
--- Basics/Chapter7Class1SalesData/main.cpp
+++ Basics/Chapter7Class1SalesData/main.cpp
@@ -1,66 +1,11 @@
 //section 7.1 defining abstract data types (Ex 7.21, p 271)
 //update your SalesData class to hide its implementation
-
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-
-class SalesData {
-	private:
-	std::string BookNO;
-	unsigned UnitsSold = 0;
-	double Revenue = 0.0;
-	
-	public:
-	std::string isbn () const {return BookNO;}
-	SalesData& Combine (const SalesData &item);
-	
-	friend ifstream &Read (ifstream &is, SalesData &item);
-	friend ostream &Write (ostream &os, const SalesData &item);
-	friend ofstream &Save (ofstream &os, const SalesData &item);
-
-};
-
-SalesData& SalesData::Combine(const SalesData &item){
-	UnitsSold += item.UnitsSold;
-	Revenue += item.Revenue;
-	return *this;
-}
-
-ifstream &Read (ifstream &is, SalesData &item);
-ostream &Write (ostream &os, const SalesData &item);
-ofstream &Save (ofstream &os, const SalesData &item);
-SalesData& Add (const SalesData item1, const SalesData item2);
-
-ifstream &Read (ifstream &is, SalesData &item){
-	double Price {0.0};
-	is >> item.BookNO >> item.UnitsSold >> Price;
-	item.Revenue = item.UnitsSold * Price;
-	return is;
-}
-
-ofstream &Save (ofstream &os, const SalesData &item){
-	os << item.BookNO <<"\t" <<item.UnitsSold <<"\t" <<item.Revenue;
-	return os;
-}
-
-ostream &Write (ostream &os, const SalesData &item){
-	os << item.BookNO <<"\t" <<item.UnitsSold <<"\t" <<item.Revenue;
-	return os;
-}
-
-SalesData& Add (const SalesData item1, const SalesData item2){
-	SalesData temp = item1;
-	temp.Combine(item2);
-	return temp;
-}
-
-
+//seperate .h from main
+#include "SalesData.h"
 
 int main(){
 	std::ifstream items {"book_sales"};
-	ofstream yeah {"book_result"};
+	std::ofstream yeah {"book_result"};
 	SalesData Total,Single;
 	if(Read(items,Total)){
 		while (Read(items,Single)){
@@ -68,14 +13,14 @@ int main(){
 				Total.Combine(Single);
 			}
 			else{
-				Write(cout,Total)<<endl; //wow, just wow for cout:)
+				Write(std::cout,Total) << std::endl; //wow, just wow for cout:)
 				Total = Single;
 			}
 		}
 		Save(yeah, Total);
 	}
 	else{
-		cerr <<"no data";
+		std::cerr <<"no data" <<std::endl;
 		return -1;
 	}
 	items.close();
