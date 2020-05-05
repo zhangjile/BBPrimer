318bc5574d5f66f87d28b244bd3003f5380a3635
Alvin-oy <schwabiz@yahoo.com>
2020-05-05 12:16:20 +0800

    SalesData constructor, cin as default argument
    Hilarious

--- Basics/Chapter7Class1SaleData/main.cpp
+++ Basics/Chapter7Class1SaleData/main.cpp
@@ -1,17 +1,14 @@
 //section 7.1 defining abstract data types (Ex 7.21, p 271)
 //update your SalesData class to hide its implementation
 //seperate .h from main
-//section 7.1, Ex 7.11,p266 add constructors to struct SalesData
-
+//section 7.5 Constructor revisited ( Ex 7.38,p291 )
+//++ std::cin as a default parameter
 #include "SalesData.h"
 
 int main(){
-	std::ifstream items {"book_sales"};
-	std::ofstream yeah {"book_result"};
-	SalesData Total(items),Single;
-	Write(std::cout,Total) << "\nTotal is custom-constructed "<<std::endl;
-	if(Read(items,Total)){ //reads the second record from book_sales file
-		while (Read(items,Single)){
+	SalesData Total, Single;
+	if(Read(std::cin,Total)){ 
+		while (Read(std::cin,Single)){
 			if(Total.isbn() == Single.isbn()){
 				Total.Combine(Single);
 			}
@@ -20,12 +17,11 @@ int main(){
 				Total = Single;
 			}
 		}
-		Save(yeah, Total);
+		Write(std::cout,Total) << std::endl; 
 	}
 	else{
 		std::cerr <<"no data" <<std::endl;
 		return -1;
 	}
-	items.close();
 	return 0;
 }
