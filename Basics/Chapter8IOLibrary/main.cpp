3812731d5e491696b2bf0e098f5811cae2b534d5
Alvin-oy <schwabiz@yahoo.com>
2020-05-13 13:32:54 +0800

    Chapter8IOLibrary PersonInfo
    istringstream
    ostringstream

--- Basics/Chapter8IOLibrary/main.cpp
+++ Basics/Chapter8IOLibrary/main.cpp
@@ -1,6 +1,41 @@
+//Ex 8.11 problem:create a vector of PersonInfo 
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <fstream>
 
-int main() {
-    std::cout << "use a template" <<std::endl;
-    return 0;
+struct PersonInfo {
+	std::string name;
+	std::vector<std::string> phone;
+};
+
+void Process (std::istream& is, std::vector<PersonInfo> &People){
+	std::string line, Phone_num;
+	while(getline(is,line)){
+		PersonInfo One;
+		std::istringstream iss(line);
+		iss >> One.name;
+		while(iss >> Phone_num){
+			One.phone.push_back(Phone_num);
+		}
+		People.push_back(One);
+	}
+}
+
+void DisplayPersonInfo(const PersonInfo& one){
+	std::cout<< one.name <<":\t ";
+	for(auto s:one.phone){
+		std::cout<<s <<", ";
+	}
+	std::cout<<std::endl;	
+}
+
+int main (int argc, char **argv){
+	std::vector<PersonInfo> People2084;
+	std::ifstream Raw (argv[1]);
+	Process(Raw, People2084);
+	for(PersonInfo p: People2084){
+		DisplayPersonInfo(p);
+	}
+	return 0;
 }
