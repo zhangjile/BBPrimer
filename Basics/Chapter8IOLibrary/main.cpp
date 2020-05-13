a53857cc1f59f5ca47683455366eee49c17b84d3
Alvin-oy <schwabiz@yahoo.com>
2020-05-13 14:17:08 +0800

    PersonInfor::Process(), a different logic
    iss.clear();
    Record.Phone = {};

--- Basics/Chapter8IOLibrary/main.cpp
+++ Basics/Chapter8IOLibrary/main.cpp
@@ -1,30 +1,35 @@
 //Ex 8.11 problem:create a vector of PersonInfo 
+// what if PersonInfo is declared and initialized outside the outer while loop?
+// reset Record.Phone to 0 before processing the next entry.
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <fstream>
 
 struct PersonInfo {
-	std::string name;
-	std::vector<std::string> phone;
+	std::string Name;
+	std::vector<std::string> Phone;
 };
 
 void Process (std::istream& is, std::vector<PersonInfo> &People){
-	std::string line, Phone_num;
+	std::string line, PhoneNumbers;
+	PersonInfo Record;
+	std::istringstream iss; //global in the function, calling clear() is critical
 	while(getline(is,line)){
-		PersonInfo One;
-		std::istringstream iss(line);
-		iss >> One.name;
-		while(iss >> Phone_num){
-			One.phone.push_back(Phone_num);
+		iss.clear();	//only the first entry is processed if this line is missing
+		iss.str(line);		
+		iss >> Record.Name;
+		while(iss >> PhoneNumbers){
+			Record.Phone.push_back(PhoneNumbers);
 		}
-		People.push_back(One);
+		People.push_back(Record);
+		Record.Phone = {};  //empty the PhoneNumbers of previous record manually
 	}
 }
 
 void DisplayPersonInfo(const PersonInfo& one){
-	std::cout<< one.name <<":\t ";
-	for(auto s:one.phone){
+	std::cout<< one.Name <<":\t ";
+	for(auto s:one.Phone){
 		std::cout<<s <<", ";
 	}
 	std::cout<<std::endl;	
@@ -34,6 +39,7 @@ int main (int argc, char **argv){
 	std::vector<PersonInfo> People2084;
 	std::ifstream Raw (argv[1]);
 	Process(Raw, People2084);
+	//Process(std::cin, People2084);
 	for(PersonInfo p: People2084){
 		DisplayPersonInfo(p);
 	}
