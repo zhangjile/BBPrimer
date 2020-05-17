9e79ab21f2b378743d224a0bd308c18c95118034
Alvin-oy <schwabiz@yahoo.com>
2020-05-17 10:19:54 +0800

    PersonInfo, std::array and assign method in action
    I bet std::array<char,12> temp
    is faster than std::string temp(12, 'c')

--- Basics/Chapter8IOLibrary/main.cpp
+++ Basics/Chapter8IOLibrary/main.cpp
@@ -2,24 +2,47 @@
 // what if PersonInfo is declared and initialized outside the outer while loop?
 // reset Record.Phone to 0 before processing the next entry.
 // validate the phone numbers
-// ++detach validation module from void Process, write a function for it
+// detach validation module from void Process, write a function for it
+// ++std::array and assign method
 
 #include <iostream>
 #include <sstream>
 #include <vector>
 #include <fstream>
+#include <array>
 
 struct PersonInfo {
 	std::string Name;
 	std::vector<std::string> Phone;
 };
 
+//test file: sstream_err
+void ValidateFormat(std::vector<PersonInfo> &People){
+	std::vector<PersonInfo> Formatted;
+	for(auto &p : People){
+		for(auto &n:p.Phone){
+			if(n.size() != 10) n = "-1"; // "-1" represent an invalid phone number
+			else {
+				std::array<char,12> temp;  //faster than std::string temp(12,'0');
+				for(int i = 0; i <4; ++i) temp[i] = n[i];
+				temp[4] = '-'; 
+				for(int i = 4; i < 7; ++i) temp[i+1] = n[i];
+				temp[8] = '-';
+				for(int i = 7; i<10; ++i) temp[i+2] = n[i];
+				n.assign(temp.begin(),temp.end()); 
+			}
+		}
+	
+	Formatted.push_back(p); //finish touch, first time work in vim 15/05/2020
+	}	
+}
+
 void Process (std::istream& is, std::vector<PersonInfo> &People){
 	std::string line, PhoneNumbers;
 	PersonInfo Record;
-	std::istringstream iss; //global in the function, calling clear() is critical
+	std::istringstream iss; //global in Process, calling clear() on it is critical
 	while(getline(is,line)){
-		iss.clear();	//ok, i feel the urgent need for vim now! 13/05/2020
+		iss.clear();	
 		iss.str(line);		
 		iss >> Record.Name;
 		while(iss >> PhoneNumbers){	
@@ -29,28 +52,6 @@ void Process (std::istream& is, std::vector<PersonInfo> &People){
 		Record.Phone = {};  
 	}
 }
-
-//test file: sstream_err
-void ValidateFormat(std::vector<PersonInfo> &People){
-	std::vector<PersonInfo> Formatted;
-	for(auto &p : People){
-		for(auto &n:p.Phone){
-			if(n.size() != 10) n = "-1"; // "-1" represent an invalid phone number
-			else {
-				std::string temp(12,'0');	//initialize!
-				for(int i = 0; i <4; ++i) temp[i] = n[i];
-				temp[4] = '-'; 
-				for(int i = 4; i < 7; ++i) temp[i+1] = n[i];
-				temp[8] = '-';
-				for(int i = 7; i<10; ++i) temp[i+2] = n[i];
-				n = temp;
-			}
-		}
-	
-	Formatted.push_back(p); //finish touch, first time work in vim 15/05/2020
-	}	
-}
-
 
 void DisplayPersonInfo(const PersonInfo& one){
 	std::cout<< one.Name <<":\t ";
