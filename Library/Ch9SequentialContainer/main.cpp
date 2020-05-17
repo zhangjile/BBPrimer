eab9cc78335ed2e8b91414ba2f2209cfa35408b3
Alvin-oy <schwabiz@yahoo.com>
2020-05-18 00:28:39 +0800

    ForwardList.cpp draft
    git as a sandbox is fabulous

--- Library/Ch9SequentialContainer/main.cpp
+++ Library/Ch9SequentialContainer/main.cpp
@@ -2,20 +2,16 @@
 #include <iostream>
 #include <vector>
 
-void double_and_insert(std::vector<int>& v, int some_val)
-{
+void double_and_insert(std::vector<int>& v, int some_val){
     auto mid = [&]{ return v.begin() +  v.size() / 2; };
-    //declare and call a function like this!?(p396:)
     for (auto curr = v.begin(); curr != mid(); ++curr)
         if (*curr == some_val)
-            //++(curr = v.insert(curr, 2 * some_val));  //equivalent
-            curr = v.insert(curr, 2 * some_val);
-            ++curr;
-
+            ++(curr = v.insert(curr, 2 * some_val)); 
 }
 
 int main(){
-	//if v.size() is odd, the code crashes on Mobile C:)
+	//if v.size() is odd, the code crashes on Mobile C
+	//test it on CodeLite and VS code
 	std::vector<int> iv = { 1, 9, 1, 9, 9,9,9, 1, 1, 1 }; 
 	double_and_insert(iv, 9);
 	for(auto it = iv.begin(); it != iv.end(); ++it){
