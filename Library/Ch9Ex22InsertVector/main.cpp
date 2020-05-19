8c0b84cc287e64373cd32796969ac54d9c62bcfe
Alvin-oy <schwabiz@yahoo.com>
2020-05-19 21:20:40 +0800

    InsertVector code didn't sustain test
    The logic of the code is problematic

--- Library/Ch9Ex22InsertVector/main.cpp
+++ Library/Ch9Ex22InsertVector/main.cpp
@@ -13,7 +13,8 @@ void double_and_insert(std::vector<int>& v, int some_val){
 }
 
 int main(){
-	std::vector<int> iv = { 1, 9, 1, 9, 9, 9, 9, 1, 1 }; 
+	//There's problem with the logic, this test case blow up everything
+	std::vector<int> iv = { 9, 9, 9, 9, 1,1, 9, 9 }; 
 	double_and_insert(iv, 9);
 	for(auto it = iv.begin(); it != iv.end(); ++it){
 		std::cout<<*it <<std::endl;
