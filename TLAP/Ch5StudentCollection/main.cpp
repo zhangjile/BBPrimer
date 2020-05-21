eaaf52368ed1834ca8f45a21c512c3fd434b3c15
Alvin-oy <zhangjile@gmail.com>
2020-05-21 05:11:03 -0400

    TLAP, StudentCollection build failure ond CodeLite

--- TLAP/Ch5StudentCollection/main.cpp
+++ TLAP/Ch5StudentCollection/main.cpp
@@ -1,163 +1,212 @@
 //Chapter 5 Solving Problems With Classes
-//flat design, well, no design pattern is applied actually.
+//design decisions: class StudentCollection;
+//work towards a better structure 
 #include<iostream>
 #include<string>
-using std::string; using std::cout; using std::endl;
+using std::cout; using std::string; using std::endl;
 
-class StudentRecord {
-	public:
-	StudentRecord();
-	StudentRecord(int Num, string Na, int Gra);
+class InvalidGrade : public std::runtime_error
+{
+public:
+    InvalidGrade() : std::runtime_error  {"Invalid grade"}
+    {}
+    ~InvalidGrade() = default;
+};
+
+class StudentRecord{
+public:
 	void SetStudentNum(int num){StudentNum = num;}
 	int GetStudentNum(){return StudentNum;}
 	void SetStudentName(string name){StudentName = name;}
 	string GetStudentName(){return StudentName;}
 	void SetStudentGrade(int grade);
-	int GetStudentGrade()const {return Grade;} //const correctness, line 35 
-    
-	private:
+	int GetStudentGrade(){return Grade;}
+	StudentRecord();
+	StudentRecord(int num, string name, int grade);
+	string LetterGrade();
+private:
 	int StudentNum;
 	string StudentName;
 	int Grade;
+	bool IsValidGrade(int num);
 };
 
 StudentRecord::StudentRecord(){
-	StudentNum = 0;
-	StudentName = "";
-	Grade = 0;
+	StudentNum = -1;
+	StudentName ="default_name";
+	Grade = -1;
 }
 
-StudentRecord::StudentRecord(int Num, string Na, int Gra)
-	:StudentNum {Num}, StudentName{Na}, Grade{Gra}{}
-	
-string LetterGrade(const StudentRecord &source){
-	if(source.GetStudentGrade() < 0 || source.GetStudentGrade() > 100) {
-		return "Error";
+bool StudentRecord::IsValidGrade(int num){
+	if(num >= 0 && num <= 100) return true;
+	else return false;
+}
+
+StudentRecord::StudentRecord(int num, string name, int grade)
+try:StudentNum {num}, StudentName {name}, Grade {grade} {
+	if(!IsValidGrade(grade)){		
+		throw InvalidGrade{}; 
 	}
+}catch (const InvalidGrade &ex){
+	std::cerr<<ex.what();
+}
+
+void StudentRecord::SetStudentGrade(int grade){
+	if(IsValidGrade(grade)){
+		Grade = grade;
+	}
+}
+
+string StudentRecord::LetterGrade(){
+	if(!IsValidGrade(Grade)) return "ERROR"; // good defense
 	const int CATEGORIES = 11;
 	const string GRADELETTERS [] = {"F", "D", "D+", "C-", "C", "C+", "B-", "B", "B+", "A-", "A"};
 	const int LOWERBOUNDS[] = {0, 60, 67, 70, 73, 77, 80, 83, 87, 90, 93};
 	size_t index = 0;
-	while(index < CATEGORIES && LOWERBOUNDS[index] <= source.GetStudentGrade()){
+	while(index < CATEGORIES && LOWERBOUNDS[index] <= Grade){ //this->Grade
 		++index;
 	}
 	return GRADELETTERS[index-1];
 }
 
-struct StudentNode {
-	StudentRecord StudentData;
-	StudentNode* next;
+class StudentCollection{
+private:
+	struct StudentNode{
+		StudentRecord StudentData;
+		StudentNode* next;
+	};
+public:
+	StudentCollection();
+	~StudentCollection();
+	StudentCollection(const StudentCollection &original); //deep copy constructor
+	StudentCollection(StudentCollection &&original); //move constructor
+	StudentCollection& operator =(const StudentCollection &rhs);
+	void AddRecord(const StudentRecord &NewRecord);
+	StudentRecord RecordWithNum(int num);
+	void RemoveRecord(int num);
+	void Print();
+private:
+	typedef StudentNode* StudentList;
+	StudentList ListHead;
+	void DeleteList(StudentList sl);
+	StudentList CopiedList(const StudentList &source);
 };
 
-typedef StudentNode* StudentList;
-
-//non-member function 
-//What a long list of parameters:)
-void AddRecord(StudentList &sl, int num, string name, int grade){
-	StudentRecord NewRecord(num,name,grade); //call constructor. COMPOSITION syntax
-	StudentNode *temp = new StudentNode; 
-	//StudentNode *temp = nullptr; //intrinsic function is not supported in Mobile C
-	if(sl != nullptr){
-		temp->StudentData = NewRecord;
-		temp->next = sl;
-	}
-	else{
-		temp->StudentData = NewRecord;
-		temp->next = nullptr;
-	}
-	sl = temp;
-	temp = nullptr; //caution 18/02/2020
+StudentCollection::StudentCollection(){
+	ListHead = nullptr;
 }
 
-StudentRecord RecordWithNum(const StudentList &sl, int num){
-	StudentNode* temp = sl;
-	while(temp != nullptr && temp->StudentData.GetStudentNum() != num){
-		temp = temp->next;
-	}
-	if(temp == nullptr){
-		StudentRecord DummyRecord(-1, "", -1);
-		return DummyRecord; //temporary error handling, trust from client code
-	}
-	return temp->StudentData;
+StudentCollection:: ~StudentCollection(){
+	DeleteList(ListHead);
 }
 
-//a brilliant and successful path/circuit
-StudentRecord Retrieve(const StudentList &sl, int num){
-	StudentNode* temp = sl;
-	while(temp != nullptr){
-		if(temp->StudentData.GetStudentNum() == num){
-			return temp->StudentData;
-			break;
-		}
-		temp = temp->next;
-	}
-	StudentRecord DummyRecord(-1, "", -1);	
-	return DummyRecord;
-}
-
-//void RemoveRecord(StudentList &sl, int num); //write the declaration, take a breath, take the time to take aim
-
-void RemoveRecord(StudentList &sl, int num){
-	StudentNode *temp = sl, *trail = nullptr;
-	if(temp->StudentData.GetStudentNum() == num){
-		sl = temp->next;
+void StudentCollection::DeleteList(StudentList sl){
+	if(sl == nullptr) return;
+	StudentNode* temp = nullptr;
+	while(sl != nullptr){
+		temp = sl;
+		sl = sl->next;
 		delete temp;
 	}
-	while(temp != nullptr && temp->StudentData.GetStudentNum() != num){
-		trail = temp;
-		temp = temp->next;
+}
+
+StudentCollection::StudentCollection(const StudentCollection &source){
+	ListHead = CopiedList(source.ListHead);
+	cout <<"deep copy constructor called"<<endl;
+}
+
+StudentCollection::StudentCollection(StudentCollection &&source){
+	ListHead = source.ListHead;
+	source.ListHead = nullptr;
+	cout <<"move constructor called for "<<endl;
+}
+
+StudentCollection& StudentCollection::operator =(const StudentCollection &rhs){
+	if(this != &rhs){ //compare 2 addresses
+		DeleteList(ListHead);
+		ListHead = CopiedList(rhs.ListHead);
+	} 	
+	return *this; 
+}
+
+//this is a nice method
+StudentCollection::StudentList StudentCollection::CopiedList(const StudentCollection::StudentList &source){
+	StudentNode* NewList = new StudentNode;
+	NewList-> StudentData = source-> StudentData;
+	StudentNode* NewLoopPtr = NewList; 
+	StudentNode* OldLoopPtr = source->next;
+	while(OldLoopPtr != nullptr){
+		NewLoopPtr->next = new StudentNode;
+		NewLoopPtr = NewLoopPtr->next;
+		NewLoopPtr->StudentData = OldLoopPtr-> StudentData;
+		OldLoopPtr = OldLoopPtr->next;
 	}
-	if(temp == nullptr){
+	NewLoopPtr->next = nullptr;
+	return NewList;
+}
+
+void StudentCollection::AddRecord(const StudentRecord &NewRecord){
+	StudentNode* temp = new StudentNode;
+	temp->StudentData = NewRecord;
+	temp->next = ListHead;
+	ListHead = temp;
+}
+
+StudentRecord StudentCollection::RecordWithNum(int idNum) {
+    StudentNode * loopPtr = ListHead;
+    while (loopPtr != NULL && loopPtr->StudentData.GetStudentNum() != idNum) {
+        loopPtr = loopPtr->next;
+    }
+    if (loopPtr == NULL) {
+      StudentRecord dummyRecord(-1, "",-1);
+        return dummyRecord;
+    } else {
+        return loopPtr->StudentData;
+    }
+}
+
+void StudentCollection::RemoveRecord(int IDNum){
+	StudentNode* LoopPtr = ListHead;
+	StudentNode* Trail = nullptr;
+	while(LoopPtr != nullptr && LoopPtr->StudentData.GetStudentNum() != IDNum){
+		Trail = LoopPtr;
+		LoopPtr = LoopPtr->next;
+	}
+	if(LoopPtr == nullptr){
 		return;
 	}
+	if(Trail == nullptr){
+		ListHead = ListHead->next;
+	}
 	else{
-		trail->next = temp->next;
+		Trail->next = LoopPtr->next;	
 	}
-	delete temp;    //"temp = nullptr; " is a fatal error that leaks.
-	trail = nullptr;
+	delete LoopPtr; //deallocate to prevent memory leak;17/2/2020 feel at home
+	Trail = nullptr; //remove double-link to be safe. 
 }
+	
 
-//an alternative approach
-void RemoveRecordShort(StudentList &sl, int num){	
-	if(sl->StudentData.GetStudentNum() == num){
-		sl = sl->next;
-	}
-	StudentNode* temp = sl, *trail = nullptr;			
-	while(temp->next != nullptr){   // offset by one position, cool, 'temp != nullptr' is  wrong. 
-		trail = temp;
-		temp = temp->next;
-		if(temp->StudentData.GetStudentNum() == num){
-			trail->next = temp->next;
-			delete temp;
-			break;	//jump out immediately with the mission completed . 
-		}
-	}
-
-}
-
-void Print(const StudentList &sl){
-	StudentNode* temp = sl;
+void StudentCollection::Print(){
+	StudentNode* temp = ListHead;
 	while(temp != nullptr){
-		cout << temp->StudentData.GetStudentName() << endl;
+		cout<<temp->StudentData.GetStudentNum()<<", "<<temp->StudentData.GetStudentName()<<", "<<temp->StudentData.GetStudentGrade()<<endl;
 		temp = temp->next;
 	}
+	cout<<endl;
 }
 
 int main(){
-	StudentRecord sr {8001, "Hot",88};
-	cout <<LetterGrade(sr)<<endl;
-	StudentList sl = nullptr;
-	AddRecord(sl, 1001, "Mike", 90);
-	AddRecord(sl, 2001, "Nike", 80);
-	AddRecord(sl, 3001, "Quark", 70);
-	AddRecord(sl, 4001, "Shark", 60);
-	Print(sl);
-	cout<<endl;
-	StudentRecord temp = RecordWithNum(sl, 3001);
-	cout<<temp.GetStudentGrade()<<endl;
-	temp = Retrieve(sl, 1005);
-	cout<<temp.GetStudentGrade()<<endl;
-	RemoveRecordShort(sl, 2001);
-	Print(sl); 
+	StudentRecord sr(2001, "Lam", 80);
+	sr.SetStudentGrade(30);
+	cout<<sr.LetterGrade()<<endl;
+	StudentCollection sc;	//sc.Print(); //display nothing:)
+	sc.AddRecord(sr);
+	sc.AddRecord(StudentRecord(3001,"Liu", 90)); 
+	sc.AddRecord(StudentRecord(4001,"Gao", 60));
+	sc.RemoveRecord(4001);
+	StudentCollection scDeepCopy = sc;
+	scDeepCopy.Print();
+	sr = sc.RecordWithNum(3001);
+	cout<<sr.GetStudentName()<<endl;
 	return 0;
 }
