28a4700e70404d237705bca0f50045e276f9ca43
Alvin-oy <schwabiz@yahoo.com>
2020-05-28 20:05:27 +0800

    sync 3 working directories
    3 identical copies

--- Library/Ch9Ex50stoi.cpp
+++ Library/Ch9Ex50stoi.cpp
@@ -5,6 +5,7 @@
 
 using std::vector; using std::string;
 
+//Ex 9.50, add up a vector of strings that represent integral value
 int AddUp(vector<string> vs){
 	int s {0};
 	for(const auto &e:vs){
@@ -29,7 +30,7 @@ public:
 Date::Date(const string &str){
 	//wash the potato before peeling
 	string numbers = "0123456789";
-	string alpha;
+	string alpha {};
 	for(char c:str){	//exlucde symbols and spaces such as , / .
 		if(isalnum(c)){
 			alpha.push_back(c);
@@ -37,9 +38,10 @@ Date::Date(const string &str){
 	}
 	for(char &c:alpha){	//tolower for precise comparison
 		c = tolower(c);
+		//std::cout << c <<"";
 	}
+	std::cout << std::endl;
 	
-	//'alpha.size() -4' is the finishing touch, wow!
 	year = stoi(alpha.substr(alpha.size()-4,4)); //the last 4 digits represent year
 	alpha.resize(alpha.size()-4);
 	
@@ -48,7 +50,7 @@ Date::Date(const string &str){
 	vector<string> m = {"jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec"};
 	
 	for(int i = 0; i < m.size(); ++i){
-		if(alpha.find(m[i])!= string::npos){
+		if(alpha.find(m[i])!= string::npos){ //if(alpha.find(m[i])), wrong!
 			month = i + 1;
 			break;
 		}		
@@ -56,11 +58,19 @@ Date::Date(const string &str){
 }
 
 void Date::DisplayDate(){
-	std::cout<<"The date: " <<month <<"/" <<day <<"/"<<year<<std::endl;
+	std::cout<<"The date: " <<month <<"/" <<day <<"/"<<year<<"\n"<<std::endl;
 }
 
 int main ()
 {
+	//Add
+	vector<string> vs {"12","34","50"};
+	std::cout <<AddUp(vs)<<std::endl;
+	
+	//Date
+	Date today("June the. /04, 1989");
+	today.DisplayDate();
+	
 	//verifying the example code
 	string name = "HongKong2020Freedom";
 	string numbers = "0123456789";
@@ -74,14 +84,6 @@ int main ()
 	
 	auto i = stoi(name.substr(name.find_first_of(numbers),2)); //2020, numbers only
 	std::cout << i << std::endl;
-	
-	//Ex 9.50, add up a vector of strings that represent integral value
-	vector<string> vs {"12","34","50"};
-	std::cout <<AddUp(vs)<<std::endl;
-	
-	//Date
-	Date today("June the. /4, 1989");
-	today.DisplayDate();
 	
 	return 0;
 }
