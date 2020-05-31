5e105e5044a78959670fe5bec01865763e89aa96
Alvin-oy <schwabiz@yahoo.com>
2020-05-31 19:44:12 +0800

    description is racing track and street corners
    ABCDEChallenge says the truth that inspirations come from reading
    descriptions

--- BB/Section19ABCDEChallenge/description.md
+++ BB/Section19ABCDEChallenge/description.md
@@ -14,9 +14,9 @@ Student2 responses
 
 Here is a sample file.
 
-ABCDE                  
-Frank                     
-ABCDE                  
+ABCDE   //string Metrics;
+Frank   //string Name;                 
+ABCDE   //string Answers    if(Answers[i] == Metrics[i]) ++Points;               
 Larry
 ABCAC
 Moe
@@ -30,14 +30,15 @@ You should read the file and display:
 Each student's name and score (#correct out of 5)
 At the end, the class average should be displayed
 You may assume that the data in the file is properly formatted
+## laugh out loud, if not, error is certain to come out.
 
 Program should output to the console the following:
-Student                     Score 
+Student                     Score   //void DisplayHeader ();
 ----------------------------
-Frank                             5
+Frank                             5     // int StudentScore(string n, string a);
 Larry                              3
 Moe                              4
 Curly                             2
 Michael                         4
 ---------------------------
-Average score            3.6
+Average score            3.6        //Average = TotalScore/StudentCount
--- BB/Section19ABCDEChallenge/main.cpp
+++ BB/Section19ABCDEChallenge/main.cpp
@@ -3,26 +3,20 @@
 #include <iomanip>
 using namespace std;
 
-//一桿子到底，哈哈大笑
-//和Frank的解決方案對比，Frank是習慣性地使用functions，模塊化并簡化代碼
 int main ()
 {
 	ifstream in_file {"responses"};
 	if (!in_file){
 		cerr<<"oops"<<endl;
-		//Boolean expression is totally effective in screening out so-called exceptions
+		//Boolean expressions are effective in intercepting exceptions
 		return 1;
 		}
-	//cout <<"good" <<endl;
-		
-	//char answers[] {};
-	//如果聲明answers是char，那麼 in_file >>answers 陣述的執行結果就是讀入一個字母
-	string answers{},name {},response{};
-	in_file >>answers;
-	//cout << answers<<endl;
-	//int length = strlen(answers);
-	int length = answers.length();	
-	//cout <<length<<endl;
+	
+	string Metrics{},Name {},Answers{};
+	in_file >>Metrics;
+//	cout << Metrics<<endl;  //primitive debugging method
+	int l = Metrics.size();	
+//	cout <<l<<endl; //the value displays 8 when data file is not formatted
 	
 	int score {0};
 	int students{0};
@@ -38,43 +32,20 @@ int main ()
     
     std::cout << std::setw(total_width) 
                    << std::setfill('-') 
-                 //  << "###" 
-    			  <<'-'
-                   << std::endl;   // display total_width dashes
+                   << "###" 
+    			//  <<'-'
+                   << std::endl;   
     std::cout << std::setfill(' '); // reset setfill to blank spaces
-    /*
-    in_file >>name>>response;
-    
-    cout<<setw(20)<<left <<name <<setw(10)<<right<< response<<endl;		
-    cout << response[2]<<endl;
-    
-    
-	in_file >>name>>response;
-		for(size_t i{0}; i<length; ++i){
-			cout<< answers[i]<<response[i]<<endl;
-		
-			}
-			cout<<setw(20)<<left <<name <<setw(10)<<right<< response<<endl;		
-			cout<<setw(20)<<left <<name <<setw(10)<<right<< score<<endl;			
-		*/
-	
-	
-	while(in_file >>name>>response){
-		for(size_t i{0}; i<length; ++i){
-			if(answers[i]==response[i])
+   
+	while(in_file >>Name>>Answers){
+		for(size_t i{0}; i<l; ++i){
+			if(Metrics[i]==Answers[i])
 				++score;
 			}
-			//cout<<setw(20)<<left <<name <<setw(10)<<right<< response<<endl;		
-			cout<<setw(20)<<left <<name <<setw(10)<<right<< score<<endl;	
+			cout<<setw(20)<<left <<Name <<setw(10)<<right<< score<<endl;	
 			++students;
-			//cout <<students<<endl;
 			total += score;
-			//cout <<total<<endl;
-			score=0;
-/********************************************************************************
-			//LOL all scores are added if this line is missing!		
-			//連滾帶爬的感覺，因為沒有使用函數來處理，前後數據粘在一起了，竟然要手動歸零。26/06/2019
-**********************************************************************************/
+			score=0;	//LOL all scores are added if this line is missing!		
 		}
 		std::cout << std::setw(total_width) 
                    << std::setfill('-') 
@@ -82,23 +53,9 @@ int main ()
     			  <<'-'
                    << std::endl;
 		std::cout << std::setfill(' ');
-cout<<setw(20)<<left <<"Average" <<setw(10)<<right<< static_cast<double>(total)/students <<endl;
-		
-		
-		/*
-	while(!in_file.eof()){
-		getline(in_file, name);
-		getline (in_file,response);
-		
-		for(size_t i {1};i<=length; ++i){
-			 if (response[i]== answers[i])
-			 	++score;			 			 		
-			 }
-			 
-		cout<<name <<response<<endl;
-		}
-		
-		*/
+		cout<<setw(20)<<left <<"Average" <<setw(10)
+			<<right<< static_cast<double>(total)/students <<endl;
+				
 	in_file.close();
 	return 0;
 	}
