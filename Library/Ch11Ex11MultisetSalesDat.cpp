d5923fac102a91cf0bc77617fa6a05f39b11072c
Alvin-oy <schwabiz@yahoo.com>
2020-06-10 22:45:57 +0800

    multiset<SalesData, ISBN> Bookstore(CompareISBN);
    Beauty

---
+++ Library/Ch11Ex11MultisetSalesDat.cpp
@@ -0,0 +1,76 @@
+﻿//Chapter 2 Variables and Basic Types
+//Ex 2.42 P77, read data from book_sales, sum up sales for each book.
+//the main function is too fat to be healthy!
+// Chapter 10.3 customizing operations Ex 12, P387
+// sort vector<SalesData> objs
+// Ex 10.17, P392 sort it using lambda expression
+// [capture list](parameter list) -> return type {function body}
+// Chapter11, Ex11.11 P426 redefine multiset Bookstore without using decltype
+	
+#include "SalesData.h"
+using ISBN = bool (*) (const SalesData &a, const SalesData &b);
+
+//vector<SalesData> MakeList (std::istream& is); //alternative modularization concept
+
+void SalesReport(vector<SalesData> &l){
+	std::ifstream book ("book_sales");
+	SalesData Total;
+	std::cout << "raw data:" << std::endl;
+	if(read(book,Total)){
+		SalesData Trans;
+		while(read(book,Trans)){
+			if(Total.isbn() == Trans.isbn()){
+				Total.combine(Trans);
+				
+			}else {
+				l.push_back(Total);
+				print(std::cout,Total) << std::endl;
+				Total = Trans;
+			}
+		}
+		l.push_back(Total);
+		print(std::cout,Total) << std::endl;
+	}
+	book.close();
+}
+
+void DisplayList(const vector<SalesData> &l){
+	for(const SalesData &e : l){
+		print(std::cout, e) <<std::endl;
+	}
+}
+
+bool CompareISBN(const SalesData &s1,const SalesData &s2){
+	return s1.isbn() < s2.isbn();
+}
+
+// Chapter11, Ex11.11
+//sort the individual sales record according to BookNo.
+void IndividualRecordsSorted (){
+	std::cout << "get individual records sorted by BookNo: " << std::endl;
+	std::multiset <SalesData, ISBN> Bookstore(CompareISBN);	
+	std::ifstream book ("book_sales");
+	//std::istream_iterator<SalesData> it(book), eof;
+	SalesData item;
+	while(read(book,item)){
+		Bookstore.insert(item);
+	}
+	for(const SalesData &e:Bookstore){
+		print(std::cout,e) <<std::endl;
+	}
+}
+
+int main()
+{
+	vector<SalesData> l;
+	SalesReport(l);
+	//sort algorithm requires random access, that's why list container didn't work
+	//trivial and significant
+		std::cout << "sorted by BookNo:" << std::endl;
+	sort(l.begin(), l.end(), [](const SalesData &s1,const SalesData &s2){return s1.isbn() <s2.isbn();});	
+	DisplayList(l);
+	
+	IndividualRecordsSorted();
+    
+    return 0;
+}
---
+++ Library/SalesData.h
@@ -0,0 +1,68 @@
+﻿//  Chapter 7 
+//  Ex 7.21
+
+#ifndef SALESDATA_H
+#define SALESDATA_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <set>
+#include <iterator>
+
+using std::string; using std::fstream; using std::vector;
+
+class SalesData {
+    friend std::istream &read(std::istream &is, SalesData &item);
+    friend std::ostream &print(std::ostream &os, const SalesData &item);
+    friend SalesData add(const SalesData &lhs, const SalesData &rhs);
+
+public:
+    SalesData() = default;
+    SalesData(const std::string &s):BookNo(s) { }
+    SalesData(const std::string &s, unsigned n, double p):BookNo(s), UnitsSold(n), Revenue(n*p){ }
+    SalesData(std::istream &is) { read(is, *this); }
+
+    std::string isbn() const { return BookNo; };
+    SalesData& combine(const SalesData&);
+
+private:
+    std::string BookNo;
+    unsigned UnitsSold = 0;
+    double Revenue = 0.0;
+};
+
+// member functions.
+SalesData& SalesData::combine(const SalesData& rhs)
+{
+    UnitsSold += rhs.UnitsSold;
+    Revenue += rhs.Revenue;
+    return *this;
+}
+
+// friend functions
+std::istream &read(std::istream &is, SalesData &item)
+{
+    double price = 0;
+    is >> item.BookNo >> item.UnitsSold >> price;
+    item.Revenue = price * item.UnitsSold;
+    return is;
+}
+
+std::ostream &print(std::ostream &os, const SalesData &item)
+{
+    os << item.isbn() << " " << item.UnitsSold << " " << item.Revenue;
+    return os;
+}
+
+//this function is not useful yet, but looks pretty.
+SalesData add(const SalesData &lhs, const SalesData &rhs)
+{
+    SalesData sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+
+#endif
