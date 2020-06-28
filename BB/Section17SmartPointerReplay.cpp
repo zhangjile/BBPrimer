18d860524462a86d44b7168943912ed2e40b4e19
Alvin-oy <schwabiz@yahoo.com>
2020-06-28 16:40:54 +0800

    Battle of Smart Pointers
    forward declaration, inline directive, macros and other build knowledge
    lead to faraway wonderful places

---
+++ BB/Section17SmartPointerReplay.cpp
@@ -0,0 +1,57 @@
+﻿//replay the smart pointer chanllenge in BB course
+//Smart Pointer is among top 5 fundamentals of modern C++, it encapsulates raw pointers, which is a huge deal
+#include <iostream>
+#include <vector>
+#include <memory>
+using std::vector; using std::unique_ptr; using std::make_unique; using std::shared_ptr; using std::make_shared; using std::cout; using std::endl; using std::cin;
+
+class Test
+{
+public:
+	Test () :data(0) {}
+	Test (int x) : data (x) {cout <<"\tconstructor called for " << data <<endl;}
+	int get() {return data;}
+	~Test () {cout << "\tTest destructor called for " << data << endl;}
+private:
+	int data;
+};
+
+typedef unique_ptr<vector<shared_ptr<Test>>> UPVS;
+
+UPVS CreateUP (){
+	//vector<shared_ptr<Test>> v;
+	//UPVS u = make_unique<vector<shared_ptr<Test>>> (v);
+	//return std::move(u);
+	return make_unique<vector<shared_ptr<Test>>> ();
+}
+void fill(vector<shared_ptr<Test>>& u, int items){
+	int d {};
+	for(int i = 1; i <= items; ++i){
+		cout<< "item [" << i <<"]: "<<endl;
+		cin >>d;
+		//u.push_back(make_shared<Test> (Test (d)));	//compare!
+		u.push_back(make_shared<Test> (d));
+	}	
+}
+
+void Display(const UPVS& u){
+	cout << "Displaying the vector:"<<endl;
+	cout << "----------------------"<<endl;
+	for(const auto &e : *u){
+		cout << e->get() << endl;
+	}
+	cout << "----------------------"<<endl;
+}
+
+int main ()
+{
+	UPVS ptr = CreateUP ();
+	int items {};
+	cout << "How many data items? " <<endl;
+	cin >>items;
+	fill(*ptr, items);
+	Display(ptr);
+	
+	return 0;
+	
+}
---
+++ Library/StrBlobPtrDraft2.cc
@@ -0,0 +1,118 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+
+using std::string; using std::vector; using std::shared_ptr; 
+using std::initializer_list; using std::make_shared; 
+using std::runtime_error; using std::out_of_range; using std::weak_ptr;
+
+class StrBlobPtr;
+
+class StrBlob {
+	friend class StrBlobPtr;	//grant friendship
+public:
+    typedef vector<string>::size_type size_type;
+    StrBlob (): data {make_shared<vector<string>> ()} {}
+    StrBlob (initializer_list<string> il): data {make_shared<vector<string>> (il)} {}
+    
+    size_type size() {return data->size();}
+    bool empty() {return data->empty();}
+    string& front() ;
+    string& back() ;
+    string& front() const {return data->front();}
+    string& back() const {return data-> back();}
+    void push_back(const string &t) {return data->push_back(t);}
+    void pop_back();
+    
+    //new methods
+    StrBlobPtr begin();
+    StrBlobPtr end();
+    
+private:
+    shared_ptr<vector<string>> data;
+    void check(size_t i, const string &msg  = "an empty vector") const;
+};
+
+string& StrBlob::front() {
+    check(0);
+    return data->front();
+}
+
+string& StrBlob::back() {
+    check(0);
+    return data-> back();
+}
+    
+void StrBlob::pop_back (){
+    check(0);
+    return data->pop_back();
+}
+
+void StrBlob::check(size_t i, const string &msg) const {
+    if(i >= data->size())
+    	throw out_of_range (msg);
+}
+
+
+//class StrBlobPtr;
+class StrBlobPtr {
+public:
+	StrBlobPtr () : curr(0) {}
+	StrBlobPtr (StrBlob &a, size_t sz = 0)
+		: wptr(a.data), curr(sz) {}
+	string& deref ();
+	StrBlobPtr& incre ();
+private:
+	shared_ptr<vector<string>> Check (size_t i, const string& msg);
+	weak_ptr<vector<string>> wptr;
+	size_t curr;
+};
+
+shared_ptr<vector<string>> StrBlobPtr::Check (size_t i, const string& msg){
+	auto it = wptr.lock();
+	if(!it){
+		throw runtime_error("unbound");
+	}
+	if(i >= it->size()){
+		throw out_of_range(msg);
+	}
+	return it;
+}
+
+
+string& StrBlobPtr::deref(){
+	auto p = Check(0, "oh");
+	return (*p)[curr];
+}
+
+StrBlobPtr& StrBlobPtr::incre (){
+	Check(curr, "oh");
+	++curr;
+	return *this;
+}
+
+StrBlobPtr StrBlob::begin() {return StrBlobPtr(*this);}
+StrBlobPtr StrBlob::end() {auto ret = StrBlobPtr(*this, data->size()); return ret;}
+
+
+int main()
+{
+    StrBlob sb1;  
+    
+    //a block of code
+    {
+    	StrBlob sb2 {"a", "an", "the"};
+    	sb1 = sb2;
+    	sb2.push_back("oh");
+    }
+    
+    std::cout<<sb1.size() <<std::endl;
+    std::cout<<sb1.back() <<std::endl;
+    
+	StrBlobPtr sbp (sb1,3);
+    sbp.deref();
+    
+    
+    return 0;
+}
