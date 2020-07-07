0a2b60451aef8342130c25236e96adb95f37a3dd
Alvin-oy <schwabiz@yahoo.com>
2020-07-07 22:03:29 +0800

    Go to bed before 11
    fine tune metablism to be most efficient in the morning

---
+++ Tools/Ch13Ex18Employee.cpp
@@ -0,0 +1,40 @@
+﻿//Chapter 13 Copy Control
+//Ex13.18, p510 Define an Employee class that contains an employee name and 
+// a unique employee identifier. Give the class a default constructor 
+// and a constructor that takes a string representing the employee's name. 
+// Each constructor should generate a unique ID by 
+// incrementing a static data member
+// first time that i write code in a text editor instead of an IDE:)
+
+#include <iostream>
+#include <string>
+
+using std::string;
+
+class Employee
+{
+static int IDGenerator;
+public:
+    //disable default synthesized constructor, 
+    //disable copy constructor and 
+    //disable copy assignment operator
+    Employee () = delete;   //disabled
+    Employee (string name) : EmployeeName {name}, EmployeeID(++IDGenerator) {}
+    Employee (const Employee& ob) = delete;     //disabled
+    Employee& operator= (const Employee& ob) = delete;  //disabled
+    
+    string GetName() {return EmployeeName;}
+    int GetID() {return EmployeeID;}
+private:
+    string EmployeeName;
+    int EmployeeID;
+};
+
+int Employee::IDGenerator = 0;
+
+int main()
+{
+    Employee One("Xu Zhang Run");
+    std::cout<<One.GetName() <<", 00" <<One.GetID() <<std::endl;
+    return 0;
+}
---
+++ Tools/Ch13Ex5HasPtr.cpp
@@ -0,0 +1,48 @@
+//Chapter 13 Copy Control
+//Ex13.5, p499, Given the following sketch of a class, wrte a copy constructor that copies all the members. Your constructor should dynamically allocate a new string and copy the object to which ps points rather than copying ps itself
+//Ex13.22, p 511, Assume that we want HasPtr to behave like a value. That is, each object should have its own copy of the string to which the object points. Write the copy constructor and copy assignment operator for class HasPtr.
+
+#include <iostream>
+#include <string>
+
+class HasPtr{
+public:
+    HasPtr(const std::string &s):
+        ps{new std::string(s)}, i{0} {}
+    //Ex 13.5
+    HasPtr (const HasPtr& source):
+        ps{new std::string(*source.ps)}, i{source.i} {}
+    //Ex13.22
+    HasPtr& operator= (const HasPtr& source){
+        //route 0
+        if(this != &source){
+            delete ps;
+            *ps = *source.ps;
+            i = source.i;            
+        }
+        return *this;
+    /*
+        //route 1
+        std::string* Newp = new std::string(*source.ps);
+        delete ps;  //deallocate ps, not destroying ps
+        ps = Newp;
+        i = source.i;
+
+        return *this;
+    */
+    }
+    void Print(){
+        std::cout << *ps <<", " << i <<std::endl;
+    }
+private:
+    std::string* ps;
+    int i;
+};
+
+int main()
+{
+    HasPtr c1("Charlie Kirk");
+    HasPtr c2 = c1;    //this is the meaning of value-like behavior, like 'x = 5'.
+    c2.Print();
+    return 0;
+}
