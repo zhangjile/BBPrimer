33ed57cefa2cf72bfb329af4046abba7734c930b
Alvin-oy <schwabiz@yahoo.com>
2020-07-24 11:14:03 +0800

    Enhance SalesData with overloaded operators
    readability improved.
    I love Rea

---
+++ ClassAuthorTools/SalesData.h
@@ -0,0 +1,68 @@
+﻿//Chapter 14 Operator Overloading
+//Ex14.2, p560, write declarations for overloaded input, output, addition and compound-assignment operators for SalesData class
+
+#ifndef SALESDATA_H
+#define SALESDATA_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+using std::string; using std::fstream; using std::vector;
+
+class SalesData {
+	//replace read, print, add friend functions with overloaded operators
+    friend std::istream & operator>>(std::istream &is, SalesData &item);
+    friend std::ostream & operator<<(std::ostream &os, const SalesData &item);
+    friend SalesData operator+(const SalesData &lhs, const SalesData &rhs);
+
+public:
+    SalesData() = default;
+    SalesData(const std::string &s):BookNo(s) { }
+    SalesData(const std::string &s, unsigned n, double p):BookNo(s), UnitsSold(n), Revenue(n*p){ }
+    SalesData(std::istream &is) { is >> *this; }
+
+    std::string isbn() const { return BookNo; };
+    //SalesData& combine(const SalesData&);
+    SalesData& operator+= (const SalesData&);
+
+private:
+    std::string BookNo;
+    unsigned UnitsSold = 0;
+    double Revenue = 0.0;
+};
+
+// member functions.
+SalesData& SalesData::operator+=(const SalesData& rhs)
+{
+    UnitsSold += rhs.UnitsSold;
+    Revenue += rhs.Revenue;
+    return *this;
+}
+
+// friend functions
+std::istream & operator>> (std::istream &is, SalesData &item)
+{
+    double price = 0;
+    is >> item.BookNo >> item.UnitsSold >> price;
+    item.Revenue = price * item.UnitsSold;
+    return is;
+}
+
+std::ostream & operator<< (std::ostream &os, const SalesData &item)
+{
+    os << item.isbn() << " " << item.UnitsSold << " " << item.Revenue;
+    return os;
+}
+
+//this function is not useful yet, but looks pretty.
+SalesData operator+(const SalesData &lhs, const SalesData &rhs)
+{
+    SalesData sum = lhs;
+    sum += rhs;
+    return sum;
+}
+
+
+#endif
---
+++ ClassAuthorTools/main.cpp
@@ -0,0 +1,50 @@
+﻿// Chapter 10.3 customizing operations Ex 12, P387
+// sort vector<SalesData> objs
+// Ex 17, P392 sort it using lambda expression
+// [capture list](parameter list) -> return type {function body}
+
+#include "SalesData.h"
+
+void SalesReport(vector<SalesData> &l){
+	std::ifstream book ("book_sales");
+	SalesData Total;
+	if(book >> Total){
+		SalesData Trans;
+		while(book >> Trans){
+			if(Total.isbn() == Trans.isbn()){
+				Total += Trans;
+				
+			}else {
+				l.push_back(Total);
+				std::cout << Total << std::endl;
+				Total = Trans;
+			}
+		}
+		l.push_back(Total);
+		std::cout << Total << std::endl;
+	}
+	book.close();
+}
+
+void DisplayList(const vector<SalesData> &l){
+	for(const SalesData &e : l){
+		std::cout << e <<std::endl;
+	}
+}
+
+bool CompareISBN(const SalesData &s1,const SalesData &s2){
+	return s1.isbn() < s2.isbn();
+}
+
+
+int main()
+{
+	vector<SalesData> l;
+	SalesReport(l);
+	//sort algorithm requires random access, that's why list container didn't work
+	//trivial and significant
+	sort(l.begin(), l.end(), [](const SalesData &s1,const SalesData &s2){return s1.isbn() <s2.isbn();});	
+	DisplayList(l);
+    
+    return 0;
+}
