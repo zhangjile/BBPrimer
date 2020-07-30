4452d05d9162817cabf35d112c4cca4efa8fbbba
Alvin-oy <schwabiz@yahoo.com>
2020-07-30 22:21:16 +0800

    get hands dirty and feet wet
    Touch the code and feel it. it's not difficulty at all if the code is
    in the palm instead of staying on pages.

---
+++ ClassAuthorTools/Ch14Ex43Modulus.cpp
@@ -0,0 +1,20 @@
+﻿//Section 14.8 Library-defined function objects
+//Ex14.43, p 576, using library function objects, determine whether a given in value is divisable by any element in a container of ints.
+
+#include <iostream>
+#include <functional>
+#include <algorithm>
+
+int main () {
+	auto c = {6, 12, 18};
+	int n;
+	std::cin >> n;
+	std::modulus<int> m;
+	auto predicate = [&](int x) {return m(x,n) == 0;};
+	
+	//test case:2,3,4,5, expected result:y,y, n,n
+	auto IsDivisable = std::all_of(c.begin(), c.end(), predicate);
+	std::cout << (IsDivisable ? "Yes" : "No" );
+	
+	return 0;
+}
---
+++ ClassAuthorTools/Ch14Ex44Binops.cpp
@@ -0,0 +1,44 @@
+//Section 14.8.3 callable objects and function template
+//Ex14.44, p 579, write your own version of a simple desk calculator that can handle binary operations 
+#include <iostream>
+#include <functional>
+#include <string>
+#include <map>
+
+using std::string; using std::map; using std::function;
+
+int Add (const int& a, const int& b){
+    return a + b;
+}
+
+class Divide {
+    int operator()(const int& a, const int& b){
+        return a/b;
+    }
+};
+
+auto mod = [](const int& a, const int& b){
+    return a % b;
+};
+
+map<string, function<int(int, int)>> binops = {
+    {"+", Add},
+    {"-", std::minus<int> ()},
+    {"*", [](int a, int b){return a*b;}},
+    {"/", Divide ()},
+    {"%", mod}
+};
+
+int main () {
+    int a = binops["+"](2020, 730);
+    std::cout << a << std::endl;
+    a = binops["-"](2020, 730);
+    std::cout << a << std::endl;
+    a = binops["*"](2020, 730);
+    std::cout << a << std::endl;
+    a = binops["/"](2020, 730);
+    std::cout << a << std::endl;
+    a = binops["%"](2020, 730);
+    std::cout << a << std::endl;
+    return 0;
+}
