9a3b68a8bb63ebc0a07918250d45d0da4f5f594a
Alvin-oy <schwabiz@yahoo.com>
2020-09-14 22:56:25 +0800

    cover more mileage on regular expressions
    seems like I've walked past the top of Himalayas. Technically speaking,
    template is the nucleus techniques upon which libraries are built,
    therefore it's the most difficult part.

---
+++ AdvancedTopics/Ch17Ex15RegexSpellingRule.cpp
@@ -0,0 +1,67 @@
+﻿//Section 17.3 Regular Expressions
+//Ex17.15, p734, write a program using the pattern that finds words that 
+// violate the "i before e except after c" rule
+// Have your pgrogram prompt the user to supply a word and indicate whether the word is okay or not. 
+//Test your program with words that do and do not violate the rule
+
+#include <iostream>
+#include <regex>
+#include <string>
+#include <chrono>
+using std::string; using std::regex; using std::smatch; using std::sregex_iterator;
+using namespace std::chrono;
+
+int main (){
+	auto StartTime = high_resolution_clock::now();
+	string pattern ("[^c]ei");
+//	pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
+	string text ="receipt freind, theif, receive, thief";
+	
+	regex r(pattern, regex::icase);
+	smatch Result;
+	while(std::cin >> text){
+		if(text == "q") break;	//yeah!
+		if(regex_search(text, Result, r)){
+			std::cout << Result.str()<<", ok" << std::endl;
+		}
+		else{
+			std::cout << "no" << std::endl;
+		}
+	}
+	
+	/* tests
+	
+	nice
+	no
+	receive
+	no
+	recieve
+	no
+	theif
+	theif, ok
+	freind
+	freind, ok
+	friend
+	no
+	
+	*/
+	
+	
+	/*
+	Ex17.16, What would happen if your regex object in the previous program were initialized with "[^c]ei"? Test your program using that pattern to see whether your expectations were correct. 
+
+	tests cases for 17.16, only 3 letters are captured
+	lei
+	lei, ok
+	theif
+	hei, ok
+	thief
+	no
+	friend
+	no
+	freind
+	rei, ok
+	q
+	*/
+	return 0;
+}
--- AdvancedTopics/Ch17Ex17RegexExample.cpp
+++ AdvancedTopics/Ch17Ex17RegexExample.cpp
@@ -13,17 +13,20 @@ int main (){
 	string text ="receipt freind, theif, receive, thief";
 	regex r(pattern, regex::icase);
 	
+	//capture the first qualified string
+	smatch Result;
+	if(regex_search(text, Result, r))
+		std::cout << Result.str() << std::endl;
+
+	//capture all the qualified strings
 	for(sregex_iterator it(text.begin(), text.end(), r),end_it; it != end_it; ++it){
 		std::cout << it->str() << std::endl;
 	}
-//	regex r(pattern);
-//	smatch Result;
-	
-//	if(regex_search(text, Result, r))
-//		std::cout << Result.str() << std::endl;
 
 	auto StopTime = high_resolution_clock::now();
     auto Duration = duration_cast<microseconds> (StopTime - StartTime);
     std::cout << Duration.count()/1000 <<std::endl;
+    
 	return 0;
 }
+
