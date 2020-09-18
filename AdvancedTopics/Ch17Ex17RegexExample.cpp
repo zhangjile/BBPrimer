8d214bab2ed05111aeb416ce9c9e04bf01c47b81
Alvin-oy <schwabiz@yahoo.com>
2020-09-18 22:04:09 +0800

    career tasks are to be completed very soon
    ios

--- AdvancedTopics/Ch17Ex17RegexExample.cpp
+++ AdvancedTopics/Ch17Ex17RegexExample.cpp
@@ -7,6 +7,10 @@ using std::string; using std::regex; using std::smatch; using std::sregex_iterat
 using namespace std::chrono;
 
 int main (){
+	//work on cin, cout and endl to be blazing fast
+	std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
 	auto StartTime = high_resolution_clock::now();
 	string pattern ("[^c]ei");
 	pattern = "[[:alpha:]]*" + pattern + "[[:alpha:]]*";
@@ -16,16 +20,17 @@ int main (){
 	//capture the first qualified string
 	smatch Result;
 	if(regex_search(text, Result, r))
-		std::cout << Result.str() << std::endl;
+		std::cout << Result.str() << "\n";
 
 	//capture all the qualified strings
 	for(sregex_iterator it(text.begin(), text.end(), r),end_it; it != end_it; ++it){
-		std::cout << it->str() << std::endl;
+		std::cout << it->str() << "\n";
 	}
-
+	//using '\n' lowers the time consumption from 53 to 50.
+	
 	auto StopTime = high_resolution_clock::now();
     auto Duration = duration_cast<microseconds> (StopTime - StartTime);
-    std::cout << Duration.count()/1000 <<std::endl;
+    std::cout << Duration.count()/1000 <<"\n";
     
 	return 0;
 }
---
+++ AdvancedTopics/Ch17Rev8_01_Tie.cpp
@@ -0,0 +1,42 @@
+﻿//	Section 8.1.3 Managing the output buffer
+
+#include <iostream>
+#include <chrono>
+using namespace std::chrono;
+#include <fstream>
+using std::cin; using std::cout; using std::istream; using std::ofstream;
+
+int main()
+{
+	std::ofstream ofs ("buffer.txt", ofstream::app);  
+	std::cout << "tie example:\n";   
+	
+	// 空参数调用返回一個指向默认的ostream object(cout)的指針  
+	*cin.tie() << "This is inserted into cout\n";  
+	*cin.tie() << "the magic of tying\n";  
+	
+	// tie cin to ofs
+	cin.tie(&ofs);
+	//std::ostream* prevstr = std::cin.tie(&ofs);  
+	
+	//returns a pointer to ofs, the ostream object tied currently  
+	*cin.tie() << "This is inserted into the file\n";  
+	
+//  cin is tied to cout again, back to default status, optional  
+//	std::cin.tie(prevstr);  
+	
+	ofs.close();  //close the file linked to ofs object.
+	
+	//test the performance of new setting
+	std::ios_base::sync_with_stdio(false);  
+	cin.tie(0);  
+	auto StartTime = high_resolution_clock::now();
+	for(int i = 0; i < (int)1e5; i++){  
+		cout<<rand()<<"\n";  
+	}  
+	auto StopTime = high_resolution_clock::now();
+	auto Duration = duration_cast<microseconds> (StopTime - StartTime);
+    std::cout << Duration.count()/1000 <<"\n";
+    
+    return 0;
+}
