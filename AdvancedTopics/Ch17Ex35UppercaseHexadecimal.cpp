611a5c29b69a5499290a96d00023c74c17f9af64
Alvin-oy <schwabiz@yahoo.com>
2020-09-19 22:08:21 +0800

    unformatted input and output operations
    make educated guesses and successfuly launched a function:)
    is.getline(sink, size, delim);

---
+++ AdvancedTopics/Ch17Ex35UppercaseHexadecimal.cpp
@@ -0,0 +1,25 @@
+﻿
+//Section 17.5.1 Formatted Input and Output
+//Ex17.35, p 760, write a version of the program from page 758, that printed the square root of 2 but this time print hexadecimal digits in uppercase
+
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+#include <cstdio>	//EOF, end of file
+
+using std::cout; 
+
+int main(){
+	cout<<std::left 
+	<<std::setw(20) << "decimal: "<<std::setw(20) <<100 * sqrt(2) <<"\n"
+	<<std::setw(20) << "scientific: " << std::scientific<<std::setw(20) <<100 * sqrt(2) <<"\n"	//02
+	<<std::setw(20) << "fixed: " << std::fixed <<std::setw(20) <<100 * sqrt(2) <<"\n"
+	<<std::setw(20) <<"hexadecimal: "<<std::uppercase <<std::hexfloat <<100*sqrt(2)<<"\n\n";	
+	//A, the result is wrong, test it on desktop
+	
+	int ch;
+	while((ch = std::cin.get()) != EOF){
+		cout.put(ch);
+	}
+	return 0;
+}
---
+++ AdvancedTopics/Ch17Ex37UnformattedGetline.cpp
@@ -0,0 +1,35 @@
+﻿//Section 17.5.2 Unformatted Input/Output Operations
+//Ex17.37, p 764, Use the unformatted version of getline to read a file a line at a time. Test your program by giving it a file that contain empty lines as well as lines that are longer than the characters that you pass to getline
+
+#include <iostream>
+#include <cmath>
+#include <fstream>
+
+using std::cin; using std::cout; 
+using std::ifstream; 
+
+int main(){
+	//returns nothing on Mobile C, test it on desktop
+	ifstream ifs("README.md");
+	char ch[100];
+	while(ifs.getline(ch, 100, '.')){
+		cout << ch[3] <<"\n";
+		cout.write(ch,100);
+	}
+	
+	/*
+	int ch;
+	while((ch = cin.get()) != EOF){
+		cout.put(ch);
+	}
+	*/
+	
+	/*
+	char ch;
+	while(cin.get(ch)){
+		cout.put(ch);
+	}
+	*/
+	return 0;
+}
+
