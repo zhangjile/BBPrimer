9b0543cec8daeb4bdac047305c72f11d33a0ab99
Alvin-oy <schwabiz@yahoo.com>
2020-09-24 23:57:36 +0800

    learning through Interview questions by CppNuts
    interview questions clarify what's right and what wrong without any
    ambiguity, it's precisely the right processing right now.

---
+++ AdvancedTopics/Nuts0924DataHiding.cpp
@@ -0,0 +1,38 @@
+﻿//CppNuts, class design::data hiding
+/*
+Question: what is data hiding?
+data hiding is specifying the access level as private or protected to a specific data member.
+data hiding is not about hacking or something, it's about correctness of expression and preventing a data member from being changed accidentally.
+*/
+//play with 100+ classes, something like getting the storyline of 100 movies
+
+#include <iostream>
+
+class ABCPlayer{
+private:
+	int volume;
+public:
+	ABCPlayer () :volume(0){}
+	void set_volume(int a){
+		if(a >= 0 && a < 100){
+			volume = a;
+			std::cout << "set" <<"\n";
+		}
+		else {
+			std::cout << "not set" <<"\n";
+		}
+	}
+	int getv(){return volume;}
+};
+
+int main (){
+	ABCPlayer ob1;
+	ob1.set_volume(200);
+	std::cout << ob1.getv() << std::endl;
+	ob1.set_volume(20);
+	std::cout << ob1.getv() << std::endl;
+	return 0;
+}
+
+// test and see what happens if default constructor is not provided
+// the program displays 20 twice, the output speaks out everything
---
+++ AdvancedTopics/Nuts0924FunctionHiding.cpp
@@ -0,0 +1,95 @@
+﻿//CppNuts, class design::function hiding
+/*
+Question: what is data hiding?
+1) If a function inherited from the Base has the same name as one in Derived, then the inherited function is hidden by the one in the Derived, it's hidden and invisible, it's unreachable by Derived objects, and is practically invisible to Derived class.
+2) a helper function usually should be detached from interface, keep it hidden and free from casual mistakes, ie, designate it as private.
+keyword: name lookup, overloading, using expression, function matching, scope resolution operator ::, sniper's techniques:)
+
+cf Section 7.4 class scope, p282
+
+
+*/
+
+/*
+//test 1
+#include <iostream>
+
+class Base{
+public:
+	void fun(int i){
+		std::cout << "Base\n";
+	}
+};
+
+class Derived : public Base {
+public:
+	void fun(char c){
+		std::cout <<"Derived\n";
+	}
+};
+
+int main (){
+	Derived d;
+	d.fun(1);	//"Derived", name lookup rule, Base::fun is hidden.
+	d.fun('a');	//"Derived"
+	return 0;
+}
+
+*/
+
+/*
+//test 2
+#include <iostream>
+
+class Base{
+public:
+	void fun() {std::cout << "Base::void\n";}
+	void fun(int i){
+		std::cout << "Base\n";
+	}
+};
+
+class Derived : public Base {
+public:
+	using Base::fun;	//bring Base::fun in the scope of the derived class
+	void fun(char c){
+		std::cout <<"Derived\n";
+	}
+};
+
+int main (){
+	Derived d;
+	d.fun(1);	//"Base"
+	d.fun('a');	//"Derived"
+	d.fun();	//"Base::void"
+	return 0;
+}
+
+*/
+
+
+//test 3
+#include <iostream>
+
+class Base{
+public:
+	void fun(int i, int j){std::cout << "Base:int, int\n";}
+	void fun(int i){
+		std::cout << "Base\n";
+	}
+};
+
+class Derived : public Base {
+public:
+	void fun(char c){
+		std::cout <<"Derived\n";
+	}
+};
+
+int main (){
+	Derived d;
+	d.Base::fun(1);	//"Base", specifying fun in Base scope to be used here
+	d.fun('a');	//"Derived"
+//	d.fun(1,2);	//error, the matching Base::fun(int,int) is hidden by Derived::fun
+	return 0;
+}
