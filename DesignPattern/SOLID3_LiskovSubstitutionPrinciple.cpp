c017b6627bddbba8579bcb14c0ee6b8218246dd9
Alvin-oy <schwabiz@yahoo.com>
2020-10-02 21:49:14 +0800

    Design Pattern, namely everyday tool of designers
    solidify basics with the help of CppNuts

---
+++ DesignPattern/SOLID3_LiskovSubstitutionPrinciple.cpp
@@ -0,0 +1,64 @@
+﻿// Objects in a program should be replaceable with instances of their subtypes
+// w/o altering the correctness of the program
+
+#include <iostream>
+
+class Rectangle
+{
+protected:
+  int width, height;
+public:
+  Rectangle(const int width, const int height)
+    : width{width}, height{height} { }
+
+  int get_width() const { return width; }
+  virtual void set_width(const int width) { this->width = width; }
+  int get_height() const { return height; }
+  virtual void set_height(const int height) { this->height = height; }
+
+  int area() const { return width * height; }
+};
+
+class Square : public Rectangle
+{
+public:
+  Square(int size): Rectangle(size,size) {}
+  void set_width(const int width) override {
+    this->width = height = width;	//'=' has right associtivity
+  }
+  void set_height(const int height) override {
+    this->height = width = height;
+  }
+};
+
+struct RectangleFactory
+{
+  static Rectangle create_rectangle(int w, int h);
+  static Rectangle create_square(int size);
+};
+
+void process(Rectangle& r)
+{
+  int w = r.get_width();
+  r.set_height(10);
+
+  std::cout << "expected area = " << (w * 10) 
+    << ", got " << r.area() << std::endl;
+}
+
+int main()
+{
+  Rectangle r{ 5,5 };
+  process(r); //process works as expected
+
+  Square s{ 5 };	//no matter what argument to the object s is
+  process(s);	//the result is permanently 100
+
+  return 0;
+}
+
+/*
+analysis:
+the derived class Square is totally not necessary and totally disastrous.
+
+*/
