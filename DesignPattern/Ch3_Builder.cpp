865796fa3959fccd06ee69de63498bffef050206
Alvin-oy <schwabiz@yahoo.com>
2020-10-11 22:17:15 +0800

    'double' becomes a little bit remote today:)
    daily exercising is essential to the workout.
    I still haven't got the very gist of builder pattern, even though the
    sample code lines are plain simple expressions.

--- DesignPattern/Ch3_Builder.cpp
+++ DesignPattern/Ch3_Builder.cpp
@@ -72,28 +72,9 @@ struct HtmlBuilder
 
 int main()
 {
-  // <p>hallo</p>
-  auto text = "hallo";
-  string output;
-  output += "<p>";
-  output += text;
-  output += "</p>";
-  cout << output << endl;
-  cout << endl;
-
-  // <ul><li>hiya</li><li>world</li></ul>
-  string words[] = { "hiya", "world" };
-  ostringstream oss;
-  oss << "<ul>";
-  for (auto w : words)
-    oss << "  <li>" << w << "</li>";
-  oss << "</ul>";
-  cout << oss.str() <<endl;
-  cout << endl;
-
   // easier
   HtmlBuilder builder{ "ul" };
-  builder.add_child("li", "hello").add_child("li", "C world");
+  builder.add_child("li", "hello").add_child("li", "Assembly");
   cout << builder.str() << endl;
   cout << endl;
 
---
+++ DesignPattern/Ch3_Builder_Basic.cpp
@@ -0,0 +1,30 @@
+﻿//This is naive implementation, upon which builders are crafted.
+#include <iostream>
+#include <string>
+#include <sstream>
+using std::string; using std::cout; using std::endl; 
+using std::ostringstream;
+
+int main()
+{
+  // <p>hallo</p>
+  auto text = "hallo";
+  string output;
+  output += "<p>";
+  output += text;
+  output += "</p>";
+  cout << output << endl;
+  cout << endl;
+
+  // <ul><li>hiya</li><li>world</li></ul>
+  string words[] = { "hiya", "world" };
+  ostringstream oss;
+  oss << "<ul>";
+  for (auto w : words)
+    oss << "  <li>" << w << "</li>";
+  oss << "</ul>";
+  cout << oss.str() <<endl;
+  cout << endl;
+  
+  return 0;
+}
