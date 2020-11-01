230c6d974473317850cb6ec308315ba75f27253a
Alvin-oy <schwabiz@yahoo.com>
2020-11-01 23:38:24 +0800

    work towards AI
    test and verify true passion honestly

--- 10 Topics/Ape.cpp
+++ 10 Topics/Ape.cpp
@@ -16,9 +16,31 @@ void PrintMatches(std::string s, std::regex reg){
     }
 }
 
+void PrintMatches2(std::string str, std::regex reg){
+	std::sregex_iterator CurrentMatch(str.begin(), str.end(),reg), End;
+	while(CurrentMatch != End){
+		std::cout <<CurrentMatch->str() <<"\n";
+		++CurrentMatch;
+	}
+	std::cout << std::endl;
+}
+
 int main(){
     std::string text = "ape at apex";
     std::regex r ("(ape[^ ]?)");
-    PrintMatches(text, r);
+//    PrintMatches(text, r);
+    
+    std::string text2 = "I picked some pickle";
+    std::regex r2("(pick([^ ]+)?)");
+//    std::regex r2("(pick[^ ]+)?");	//blank lines    
+    PrintMatches2(text2, r2);
+    
+    std::string text3 = "Cat, fat, mat, rat";
+    std::regex r3("[cfm]at");
+    PrintMatches2(text3, r3);
+    
+    r3 = "[C-Fc-f]at";
+    PrintMatches2(text3, r3);
+    
     return 0;
 }
