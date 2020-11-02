007081ca74509f64a66b1d5cdcb1c0cb66f3c9ad
Alvin-oy <schwabiz@yahoo.com>
2020-11-02 23:28:45 +0800

    Election day tomorrow
    Donald J. Trump, four more years!

--- 10 Topics/Ape.cpp
+++ 10 Topics/Ape.cpp
@@ -28,7 +28,7 @@ void PrintMatches2(std::string str, std::regex reg){
 int main(){
     std::string text = "ape at apex";
     std::regex r ("(ape[^ ]?)");
-//    PrintMatches(text, r);
+    PrintMatches(text, r);
     
     std::string text2 = "I picked some pickle";
     std::regex r2("(pick([^ ]+)?)");
@@ -41,6 +41,38 @@ int main(){
     
     r3 = "[C-Fc-f]at";
     PrintMatches2(text3, r3);
+    
+    std::string OwlFood = regex_replace(text3,r3,"owl");
+    std::cout << OwlFood <<"\n";
+    
+    std::string text4 = "F.B.I. I.R.S CIA";
+    std::regex r4("([^ ]\\..\\..)");
+    PrintMatches2(text4, r4);
+    
+    std::string text5 = "This is\n a multiline" 
+    	"\nsentence.";
+    std::regex r5("\n");
+    std::string s5 = std::regex_replace(text5, r5, " ");
+    std::cout << s5 <<"\n";
+    
+    std::string text6 = "123Calculus45";
+    std::regex r6("\\D");
+//  std::regex r6("\\d");  
+    PrintMatches2(text6, r6);
+    
+    std::string text7 = "123 12345 123456 1234567 12345678   ";
+    std::regex r7("\\d{5,7}");
+//    std::regex r7("\\d{5}");
+    PrintMatches2(text7, r7);
+    
+    std::string text8 = "852-123-4567";
+    std::regex r8("\\d{3}-\\d{3}-\\d{4}");	//w
+    PrintMatches2(text8, r8);
+    
+    std::string text9 = "a and an are not determinantooooo";
+//    std::regex r9("a[a-z].");
+    std::regex r9("a[a-z]+");
+    PrintMatches2(text9, r9);
     
     return 0;
 }
---
+++ AdvancedTopics/Geek1102regex_match.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream> 
+#include <regex> 
+  
+using namespace std; 
+
+// C++ program to demonstrate working of regex_match() 
+void Match(){
+	string a = "GeeksForGeeks"; 
+  
+    // Here b is an object of regex (regular expression) 
+    regex b("(Geek)(.*)"); // Geeks followed by any character 
+  
+    // regex_match function matches string a against regex b 
+    if ( regex_match(a, b) ) 
+        cout << "String 'a' matches regular expression 'b' \n"; 
+  
+    // regex_match function for matching a range in string  
+    // against regex b 
+    if ( regex_match(a.begin(), a.end(), b) ) 
+        cout << "String 'a' matches with regular expression "
+                "'b' in the range from 0 to string end\n"; 
+  	else
+  		cout << "String 'a' doesn't match regular expression 'b' \n"; 
+  
+}
+
+void Replace(){
+	string s = "I am looking for GeeksForGeek \n"; 
+      
+    // matches words beginning by "Geek" 
+    regex r("Geek[a-zA-z]+"); 
+      
+    // regex_replace() for replacing the match with 'geek'  
+    cout << std::regex_replace(s, r, "geek"); 
+      
+    string result; 
+      
+    // regex_replace( ) for replacing the match with 'geek' 
+    regex_replace(back_inserter(result), s.begin(), s.end(), 
+                  r,  "geek"); 
+  
+    cout << result; 
+}
+
+int main() 
+{ 
+    Match();
+    Replace();
+    return 0; 
+} 
