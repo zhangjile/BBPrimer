335dcd85e7637b67278054f2f411d0398b2ce6ee
Alvin-oy <schwabiz@yahoo.com>
2020-11-05 22:39:16 +0800

    grasp the meaning of . * + ?
    socket goes in the direction of computer system, it's intriguing,
    perhaps not for now

--- 10 Topics/Ape3.cpp
+++ 10 Topics/Ape3.cpp
@@ -27,10 +27,20 @@ int main(){
     std::string text1 = "<a href='#'> <b> this is a link </b></a>";
     std::regex r1 ("<b>(.*)</b>");
     std::string output;
+    
+    //this quoting technique is also called back reference
     std::regex_replace(std::back_inserter(output), text1.begin(), text1.end(), r1, "$1");
     std::cout <<output << "\n";
     
-    
+    //challenge insert link data into html code
+    //first time to see $2 inside $1
+    //using . and + the expression pattern define a link as an entity, 
+    std::string text2 = "https://www.google.com http://www.youtube.com";
+    std::regex r2("(https?://([\\w.]+))");
+    std::string fmt = "<a href = '$1'>$2</a>\n";
+    std::string output2;
+    std::regex_replace(std::back_inserter(output2), text2.begin(), text2.end(), r2, fmt);
+    std::cout <<output2 << "\n";
     
     return 0;
 }
