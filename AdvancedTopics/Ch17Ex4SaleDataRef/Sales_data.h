67107fd0e3df34fd3adb841ef6b4e2cd4eefa143
Alvin-oy <schwabiz@yahoo.com>
2020-11-14 11:01:35 +0800

    documenting SalesData project
    release version is kept

--- AdvancedTopics/Ch17Ex4SaleDataRef/Sales_data.h
+++ AdvancedTopics/Ch17Ex4SaleDataRef/Sales_data.h
@@ -1,31 +1,6 @@
 ﻿/*
  * This file contains code from "C++ Primer, Fifth Edition", by Stanley B.
- * Lippman, Josee Lajoie, and Barbara E. Moo, and is covered under the
- * copyright and warranty notices given in that book:
- * 
- * "Copyright (c) 2013 by Objectwrite, Inc., Josee Lajoie, and Barbara E. Moo."
- * 
- * 
- * "The authors and publisher have taken care in the preparation of this book,
- * but make no expressed or implied warranty of any kind and assume no
- * responsibility for errors or omissions. No liability is assumed for
- * incidental or consequential damages in connection with or arising out of the
- * use of the information or programs contained herein."
- * 
- * Permission is granted for this code to be used for educational purposes in
- * association with the book, given proper citation if and when posted or
- * reproduced.Any commercial use of this code requires the explicit written
- * permission of the publisher, Addison-Wesley Professional, a division of
- * Pearson Education, Inc. Send your request for permission, stating clearly
- * what code you would like to use, and in what specific way, to the following
- * address: 
- * 
- * 	Pearson Education, Inc.
- * 	Rights and Permissions Department
- * 	One Lake Street
- * 	Upper Saddle River, NJ  07458
- * 	Fax: (201) 236-3290
-*/ 
+ * Lippman, Josee Lajoie, and Barbara E. Moo */ 
 
 
 #ifndef SALES_DATA_H
@@ -84,9 +59,7 @@ struct hash<Sales_data>  // the template parameter of Sales_data
 
     size_t operator()(const Sales_data& s) const;
 
-    // our class uses synthesized copy control and default constructor
-    // other members as before
-};
+    };
 }  // close the std namespace; note: no semicolon after the close curly
 
 // non-member Sales_data operations
