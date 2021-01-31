06a7884de7619c6c838f2e57329ca037015ac4ce
Alvin-oy <schwabiz@yahoo.com>
2021-01-31 18:15:30 +0800

    sensational gameplay
    always find a way out!

---
+++ Basics/Chapter6Functions/ReturningAFunctionPointer.cpp
@@ -0,0 +1,22 @@
+﻿//finally a function that returns a pointer to another function
+#include <iostream>
+
+typedef int (*op_func)(int, int);
+
+int add (int a, int b){ return a+b; }
+int sub (int a, int b){ return a-b; }
+
+op_func getfunc(char op){
+    switch(op) {
+        case '+': return add;
+        case '-': return sub;
+        default:
+        	return add;
+    };
+}
+
+int main(){
+    printf("5 + 7 = %d\n", getfunc('+')(5, 7));
+    printf("5 - 7 = %d\n", getfunc('-')(5, 7));
+	return 0;
+}
--- Library/Ch11Ex32NovelsAlphabetically/main.cpp
+++ Library/Ch11Ex32NovelsAlphabetically/main.cpp
@@ -7,17 +7,60 @@
 using std::map; using std::multimap; using std::string; using std::pair;
 using std::set;
 
-
-void PrintAlphabetically(const multimap<string, string> &m){
-	map<string, set<string>> p;
-	for(const auto &e:m){
-		p[e.first].insert(e.second);
-	}
+//module of utilities, seperation of concerns, avoiding duplicate code
+using mss = map<string, set<string>>;
+void PrintMapOfStringSet(mss p){
 	for(const auto &e:p){
 		for(const auto &o:p[e.first]){
 			std::cout <<e.first <<": " <<o << std::endl;
 		}
 	}
+}
+
+void PrintAlpha1(const multimap<string, string> &m){
+	mss p;
+	for(const auto &e:m){
+		p[e.first].insert(e.second);
+	}
+	PrintMapOfStringSet(p);
+}
+
+void PrintAlpha2 (const multimap<string, string> & Authors){
+	mss Ordering;
+	for(auto beg = Authors.begin(); beg != Authors.end(); ++beg){
+		Ordering[beg->first].insert(beg->second);
+	}
+
+	PrintMapOfStringSet(Ordering);
+}
+
+//hahahahahahah, laugh my butt off. What a sensational gameplay!
+void PrintAlpha3 (const multimap<string, string> & Authors){
+	string name = Authors.begin()->first;
+	mss Ordering;
+	for(auto range = Authors.equal_range(name);range.first != Authors.end();){
+		set<string> works {};
+		//handling exit condition in the first place
+		// but 'range.first != Author.end()'
+		if(range.second == Authors.end()){
+			while(range.first != range.second){
+			works.insert(range.first->second);
+			++range.first;
+			}
+			Ordering.insert({name, works});
+			range.first = Authors.end();
+		}
+		else{
+			while(range.first != range.second){
+				works.insert(range.first->second);
+				++range.first;
+			}
+			Ordering.insert({name, works});
+			name = range.second->first;
+			range = Authors.equal_range(name);
+		}				
+	}
+	PrintMapOfStringSet(Ordering);
 }
 
 int main()
@@ -29,7 +72,7 @@ int main()
 		{"Clint Eastwood","Gunman"}
 	};
 	
-	PrintAlphabetically(authors);
+	PrintAlpha3(authors);
 	
 	return 0;
 }
