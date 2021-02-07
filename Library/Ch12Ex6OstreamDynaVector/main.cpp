52476795fcf7fb408c24bf9ef6abe51de4483fd8
Alvin-oy <schwabiz@yahoo.com>
2021-02-07 12:27:29 +0800

    Qt will be my  C++ framework
    I'll be moving into Qt after this round of trip. one code base for all
    platforms

--- Library/Ch12Ex6OstreamDynaVector/main.cpp
+++ Library/Ch12Ex6OstreamDynaVector/main.cpp
@@ -5,10 +5,11 @@
 
 #include <iostream>
 #include <vector>
+#include <string>
 #include <memory> 
 
 using std::vector; using std::cout; using std::shared_ptr; using std::make_shared;
-
+using std::string; using std::endl;
 typedef shared_ptr<vector<int>> piv;
 
 piv DynamicVector (){
@@ -16,24 +17,72 @@ piv DynamicVector (){
 }
 
 piv BuildVector(piv p){
-	for(int i = 0; cout <<"Enter here: ", std::cin >> i;){
+	for(int i = 0; cout <<"Enter here: ", std::cin >> i, i != 1989;){
 		p->push_back(i);
 	}
 	return p;
 }
 
 //this print function is colorful.
-auto Print(piv p) -> std::ostream&{	//if '&' is missing, ERROR!
+//auto Print(piv p) -> std::ostream&{	//if '&' is missing, ERROR!
+ std::ostream& Print(piv p){	
 	for(const auto &i : *p){
 		cout << i << " ";
 	}
 	return cout;
 }
 
+void TestReset466(){
+	shared_ptr<int> p(new int(42));
+	shared_ptr<int> p1(p);
+	cout <<"\nUse count of p: " <<p.use_count() << endl;
+	if(!p.unique()){
+		p.reset(new int(*p));
+		cout <<"executed" <<endl;	//confirming 'p.use_count() == 2'
+	}
+	cout << p.use_count() << endl;
+}
+
+
+//Ch12Ex6, this is pure gameplay, use smart pointers exclusively!
+using std::istream; using std::cin;
+using vip = vector<int>*;
+
+vip pv(){
+	auto ptr(new vector<int>());
+	return ptr;
+}
+
+vip Initialize(vip p, istream& is = cin){
+	int i = 0;
+	while(is >> i){
+		p->push_back(i);
+	}
+	return p;
+}
+
+void Print(vip p){
+	for(const int& i : *p){
+		cout << i << " ";
+	}
+	cout << endl;
+	
+	//the caller is responsible for deallocating the heap memory and cleaning up.
+	//this is the key, destroy the link, free the memory and 
+	//it is destroyed after the block is executed
+	delete p;	
+}
+
 int main ()
 {
 	piv p = BuildVector(DynamicVector ());
-	Print(p) << std::endl;
 	//shared_ptr handles the dynamic memory properly!
+	Print(p) << std::endl;	
+	TestReset466();
+	
+	//Ex6
+	vip p_raw = pv();
+	vip p_data = Initialize(p_raw);
+	Print(p_data);
 	return 0;
 }
