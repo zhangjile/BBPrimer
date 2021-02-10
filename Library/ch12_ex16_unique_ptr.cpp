b14d43e93584bd0746dd8d2c61006263566b4196
Alvin-oy <schwabiz@yahoo.com>
2021-02-10 14:31:38 +0800

    snake_case is prefered for readability from now on
    it's much easier to read than Pascal. and it looks humble:)

---
+++ Library/ch12_ex16_unique_ptr.cpp
@@ -0,0 +1,44 @@
+﻿//Ch12.1.5 unique_ptr, p472, Ex16 
+//orchestrate an error for copying or assigning a unique pointer.
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+using std::string;
+typedef std::unique_ptr<string> ups; 
+
+void experimentP470 () {
+	//put it on heap
+	std::unique_ptr<string> ps1 (new string("Jose Mourinho")); 	
+	
+	/*
+	//orchestrate an error
+	auto err = ps1;
+	//call to implicitly-deleted copy constructor of
+      'unique_ptr<basic_string<char>,
+      default_delete<basic_string<char> > >'
+	*/
+	
+	auto p1 = ps1.get();	//inspect the raw ptr managed by ps1
+	std::cout <<"p1: "<< p1 << std::endl;
+	ups ps2(ps1.release());	//p1 is tranfered to ps2
+	auto p2 = ps2.get();	//inspect the raw ptr managed by ps2
+	std::cout <<"p2: "<< p2 << std::endl;
+	
+	ups ps3 (new string("Tottenham"));
+	auto p3 = ps3.get();	
+	ps2.reset(ps3.release());	//p3 is transfered to ps2
+	p2 = ps2.get();
+	std::cout <<"p2: "<< p2 << std::endl;	//p2 has been changed
+	// good practice to reset it to nullptr
+	// 'u.reset()' deletes the object u points but doesn't RESET
+	ps2.reset(nullptr);	
+	
+}
+
+
+int main(){
+	experimentP470();
+	return 0;
+}
+
---
+++ Library/ch12_ex26_allocator.cpp
@@ -0,0 +1,75 @@
+﻿//Ch12Ex26, p484, Rewrite the program on page 481 using an allocator
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <memory>
+using std::string;
+typedef std::unique_ptr<string> ups; using std::vector; using std::allocator;
+
+//the allocator class, p481
+void new_alloc_initi (int n){
+	string *p= nullptr;
+	p = new string[n];
+	string *q = p;
+	string s;
+	while(std::cin >> s && q!= p+n)
+		*q++ = s;
+	const size_t size = p-q;
+	for(size_t i = 0; i < p-q; ++i){
+		std::cout << p[i] << ", ";
+	}
+	std::cout << std::endl;
+	delete[] p;
+}
+
+void alloc_only(int n){
+	allocator<string> alloc;
+	auto const p = alloc.allocate(n);
+	auto q = p;
+	string s;
+	
+//	while(std::cin >> s && s != "q" && q!= p+n) //short-circuit evaluation
+	while(s != "q" && q!= p+n && std::cin >> s)
+		alloc.construct(q++,s);
+	const size_t size = q-p;
+	for(size_t i = 0; i < q-p; ++i){
+		std::cout << *(p+i) << " ";
+	}
+	std::cout << std::endl;
+	
+	while(q != p){
+		alloc.destroy(--q);
+	}
+	
+	alloc.deallocate(p, n);
+	
+}
+
+void alloc_example (){
+	allocator<int> alloc;
+	auto const p = alloc.allocate(10);	//const specifier is significant
+	auto q = p;
+	alloc.construct(q++, 1);
+	alloc.construct(q++, 9);
+	alloc.construct(q++, 8);
+	alloc.construct(q++, 9);
+	std::cout << *p << std::endl;
+	
+	q = std::uninitialized_fill_n(q,3,6);
+	std::uninitialized_fill_n(q,3,4);
+	std::cout << *q << std::endl;
+	while(q != p){
+		alloc.destroy(--q);
+	}
+	alloc.deallocate(p, 10);
+}
+
+int main(){
+//	experimentP470();
+//	alloc_example();
+	int n = 5;
+	alloc_only(n);
+	return 0;
+}
+
