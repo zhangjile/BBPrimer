5a6a96be13e3e1159186e1df6f40f396a724d98b
Alvin-oy <schwabiz@yahoo.com>
2021-04-28 10:00:07 +0800

    walk through template comfortably
    reference collasing, move, forward, variadic parameter pack are easy
    games.

--- ClassAuthorTools/Ch16Ex47Flip.cpp
+++ ClassAuthorTools/Ch16Ex47Flip.cpp
@@ -5,32 +5,54 @@
 #include <iostream>
 #include <utility>
 
+//part 1
+void fa(int a, int& b){
+	std::cout << a <<":" <<++b <<std::endl;
+}
+
 template<typename F, typename T1, typename T2>
 void FlipA(F f, T1 t1, T2 t2){
-    f(t2,t1);	//primitive
+    f(t2,t1);	//an unexpected result pops out.
 }
 
-template<typename F, typename T1, typename T2>
-void FlipB(F f, T1&& t1, T2&& t2){
-	//water-proof forwarding that keeps all the type information
-    f(std::forward<T2> (t2), std::forward<T1> (t1));	
-}
-
-//L-value/R-value nature of the argument is preserved
-void f(int&& a, int& b){
+//part 2
+void fb(int&& a, int& b){
     std::cout << a << ", " << ++b << std::endl;
 }
 
-int main (){
-    int x = 8;
-//    f(42, x);
-//    std::cout << x << std::endl;
+template<typename F, typename T1, typename T2>
+void FlipB(F f, T1&& t1, T2&& t2){ //half baked
+	f(t2, t1);	
+}
 
-//    FlipA(f, x, 42);
-//    std::cout << x << std::endl;
-    
-    FlipB(f, x, 42);
+//part 3
+template<typename F, typename T1, typename T2>
+void Flip(F f, T1&& t1, T2&& t2){
+	//done, water-proof, forwarding keeps the argument type information
+    f(std::forward<T2> (t2), std::forward<T1> (t1));	
+}
+
+int main (){	
+    int x = 8, y=8;
+    //testing part 1
+    std::cout << "testing part 1" << std::endl;
+    fa(42, x);
     std::cout << x << std::endl;
+
+    FlipA(fa, y, 42);
+    std::cout << y << std::endl;
     
+    //testing part 2
+//    FlipB(fb, y, 42);	//error
+    
+   //testing part 3
+   //reset 
+   x =8; 
+   y =8;
+    
+   std::cout << "testing part 3" << std::endl;    
+   Flip(fb, y, 42);
+   std::cout << y << std::endl;
+
     return 0;
 }
